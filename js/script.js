39aa05de166dc4002dd1a58c598ded25a0cbc9b0
Anna <aaaananas1@gmail.com>
2023-02-10 13:21:12 +0200

    fix

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// console.log(window.screen.availHeight);
+// console.log(window.devicePixelRatio);
+
+// console.log(window);
+
+const windowInnerHeight = document.documentElement.clientHeight
+console.log(windowInnerHeight);
+
+// const pageHeight = document.documentElement.scrollHeight
+// console.log(pageHeight);
+
+// const body = document.querySelector('.container').clientHeight;
+
+// console.log(body);
+
+// console.log(window.visualViewport);
+
+
+
